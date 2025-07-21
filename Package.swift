78e918b5e1a4750417bc6081e5365aff49d0b0fe
Ivan Sapozhnik <sapozhnik.ivan@gmail.com>
2025-07-21 21:07:34 +0200

    initial template

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NimbusUI",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "NimbusUI",
+            targets: ["NimbusUI"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "NimbusUI"),
+        .testTarget(
+            name: "NimbusUITests",
+            dependencies: ["NimbusUI"]
+        ),
+    ]
+)
